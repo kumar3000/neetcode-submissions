1934366e6aee27029a8224304000a4ce9be15ef7
Sidarth Santhosh Kumar <sidarthoscar15@gmail.com>
2026-04-29 17:48:49 -0400

    Add: duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn has_duplicate(nums: Vec<i32>) -> bool {
+        let mut unique = HashSet::new();
+        for num in nums {
+            if unique.contains(&num) {
+                return true;
+            } else {
+                unique.insert(num);
+            }
+        }
+        return false;
+    }
+}
